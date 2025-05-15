3ab7c68e42b116ac4259f95d733d9509d9e90b54
Ujjwal Kriti <tanukriti06@gmail.com>
2025-05-15 19:44:33 +0530

    Time: 0 ms (100%), Space: 31.4 MB (9.59%) - LeetHub

--- 0792-binary-search/0792-binary-search.cpp
+++ 0792-binary-search/0792-binary-search.cpp
@@ -1,15 +1,29 @@
 class Solution {
 public:
-    int search(vector<int>& nums, int target) {
-       int n=nums.size();
-       int low=0,high=n-1;
-       while(low<=high){
-        int  mid=low+(high-low)/2;
-        if(target==nums[mid]) return mid;
-        else if(target>nums[mid]) low=mid+1;
-        else high=mid-1;
-       }
-          return -1;
+    // int search(vector<int>& nums, int target) {
+    //    int n=nums.size();
+    //    int low=0,high=n-1;
+    //    while(low<=high){
+    //     int  mid=low+(high-low)/2;
+    //     if(target==nums[mid]) return mid;
+    //     else if(target>nums[mid]) low=mid+1;
+    //     else high=mid-1;
+    //    }
+    //       return -1;
+    // }
+
+    // recursive approach 
+    int bs(vector<int>&nums,int low,int high,int target){
+        //base case
+        if(low>high) return -1;
+        int mid=low+(high-low)/2;
+        if(nums[mid]==target) return  mid;
+        else if(target>nums[mid])
+        return bs(nums,mid+1,high,target);
+        else
+        return bs(nums,low,mid-1,target);
     }
-  
+    int search(vector<int>&nums,int target){
+        return bs(nums,0,nums.size()-1,target);
+    }
 };
