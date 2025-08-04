02d2fbd11bdfcd21bca2b088091b6b51645699f9
Ujjwal Kriti <tanukriti06@gmail.com>
2025-08-04 11:38:35 +0530

    Time: 15 ms (13.98%), Space: 138.8 MB (31.72%) - LeetHub

--- 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.cpp
+++ 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.cpp
@@ -1,37 +1,19 @@
 class Solution {
 public:
-    vector<int> rearrangeArray(vector<int>& a) {
-        
-        // brute force approach
-
-        // vector<int>pos;
-        // vector<int>neg;
-        // int n=a.size();
-        // for(int i=0;i<n;i++ ){
-        //     if(a[i]>0) pos.push_back(a[i]);
-        //     else  neg.push_back(a[i]);
-        // }
-        // for(int i=0;i<n/2;i++){
-        //    a[2*i]= pos[i];
-        //    a[2*i+1]=neg[i];
-        // }
-        // return a;
-
-        // ***************optimal approach***********
-        int n=a.size();
-        vector <int>ans(n,0);
-        int pos_index=0;
-        int neg_index=1;
-        for(int i=0;i<n;i++){
-            if(a[i]>0){
-                ans[pos_index]=a[i];
-                pos_index+=2;
-            }else{
-                ans[neg_index]=a[i];
-                neg_index+=2;
+    vector<int> rearrangeArray(vector<int>& nums) {
+        vector<int>pos;
+        vector<int>neg;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]>0)pos.push_back(nums[i]);
+            else{
+               neg. push_back(nums[i]);
             }
+
         }
-        return ans;
-        
+        for(int i=0;i<nums.size()/2;i++){
+                nums[2*i]=pos[i];
+                nums[2*i+1]=neg[i];
+        }
+        return nums;
     }
 };
