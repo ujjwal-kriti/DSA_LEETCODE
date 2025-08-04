5f855a3c115223e219c94ded131bbac175b0c280
Ujjwal Kriti <tanukriti06@gmail.com>
2025-08-04 15:07:02 +0530

    Time: 0 ms (100%), Space: 128.3 MB (79.34%) - LeetHub

--- 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.cpp
+++ 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.cpp
@@ -1,19 +1,35 @@
 class Solution {
 public:
     vector<int> rearrangeArray(vector<int>& nums) {
-        vector<int>pos;
-        vector<int>neg;
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]>0)pos.push_back(nums[i]);
-            else{
-               neg. push_back(nums[i]);
-            }
+        // vector<int>pos;
+        // vector<int>neg;
+        // for(int i=0;i<nums.size();i++){
+        //     if(nums[i]>0)pos.push_back(nums[i]);
+        //     else{
+        //        neg. push_back(nums[i]);
+        //     }
 
+        // }
+        // for(int i=0;i<nums.size()/2;i++){
+        //         nums[2*i]=pos[i];
+        //         nums[2*i+1]=neg[i];
+        // }
+        // return nums;
+
+        //***************uisng two pointer approach
+        int n=nums.size();
+        vector <int>ujj(n);
+        int pos=0,neg=1;
+        for(int i=0;i<n;i++){
+            if(nums[i]>0){
+            ujj[pos]=nums[i];
+            pos+=2;
+            }
+            else{
+                ujj[neg]=nums[i];
+                neg+=2;
+            }
         }
-        for(int i=0;i<nums.size()/2;i++){
-                nums[2*i]=pos[i];
-                nums[2*i+1]=neg[i];
-        }
-        return nums;
+        return ujj;
     }
 };
