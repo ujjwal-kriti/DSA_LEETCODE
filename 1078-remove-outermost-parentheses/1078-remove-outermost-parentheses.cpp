78eb649a394fe88d609250fa32d4ab4e172822df
Ujjwal Kriti <tanukriti06@gmail.com>
2025-08-08 17:06:49 +0530

    Time: 3 ms (19.39%), Space: 9.1 MB (24.22%) - LeetHub

--- 1078-remove-outermost-parentheses/1078-remove-outermost-parentheses.cpp
+++ 1078-remove-outermost-parentheses/1078-remove-outermost-parentheses.cpp
@@ -1,21 +1,30 @@
 class Solution {
 public:
     string removeOuterParentheses(string s) {
-        int depth=0;
-        string result;
-         for(char c: s){
-               if(c=='('){
-                    if(depth>0){
-                        result+=c;
-                    }
-                    depth++;
-               } else{// c==')
-                depth--;
-                if(depth>0){
-                    result+=c;
-                }
-               }
-            }
-    return result;
+//         int depth=0;
+//         string result;
+//          for(char c: s){
+//                if(c=='('){
+//                     if(depth>0){
+//                         result+=c;
+//                     }
+//                     depth++;
+//                } else{// c==')
+//                 depth--;
+//                 if(depth>0){
+//                     result+=c;
+//                 }
+//                }
+//             }
+//     return result;
+//     }
+
+    int depth=0;
+    string res;
+    for(char c: s){
+        if(c=='(' && depth++> 0)  res+=c;
+        if(c==')' && --depth > 0) res+=c;
+    }
+    return res;
     }
 };
