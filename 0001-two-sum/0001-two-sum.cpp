dda468c66e3b7e9cc38517f4fa59027a4a01de7f
Ujjwal Kriti <tanukriti06@gmail.com>
2025-08-09 08:51:39 +0530

    Time: 34 ms (40.7%), Space: 14.1 MB (78.21%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -4,12 +4,12 @@ public:
         int n=nums.size();
         for(int i=0;i<n;i++){
             for(int j=i+1;j<n;j++){
-                if(nums[i] + nums[j]==target){
-                    return{i,j};
+                if(nums[i]+nums[j]==target){
+                    return {i,j};
                 }
             }
         }
-         return {};
+        return {};
     }
    
 };
