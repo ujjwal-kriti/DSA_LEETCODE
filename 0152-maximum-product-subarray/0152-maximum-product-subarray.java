d78dfa982e5bd13630b76220a3a60d5372d94781
Ujjwal Kriti <tanukriti06@gmail.com>
2025-10-06 12:59:20 +0530

    Time: 2 ms (55.25%), Space: 45.5 MB (7.74%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.java
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.java
@@ -1,13 +1,38 @@
 class Solution {
     public int maxProduct(int[] nums) {
-        int maxp=Integer.MIN_VALUE;
-        for(int i=0;i<nums.length;i++){
-            int product=1;
-            for(int j=i;j<nums.length;j++){
-                product*=nums[j];
-                maxp=Math.max(product,maxp);
-            }
+        // brute force approach 
+
+        // int maxp=Integer.MIN_VALUE;
+        // for(int i=0;i<nums.length;i++){
+        //     int product=1;
+        //     for(int j=i;j<nums.length;j++){
+        //         product*=nums[j];
+        //         maxp=Math.max(product,maxp);
+        //     }
+        // }
+        // return maxp;
+
+
+        // optimal approach using kadanes algorithm
+
+        
+        int maxprod=nums[0];
+        int minprod=nums[0];
+        int ans=nums[0];
+
+        for(int i=1;i<nums.length;i++){
+            int currprod=nums[i];
+    
+        if(currprod<0){
+            int temp=maxprod;
+            maxprod=minprod;
+            minprod=temp;
         }
-        return maxp;
+        maxprod=Math.max(maxprod*currprod,currprod);
+        minprod=Math.min(minprod*currprod,currprod);
+
+        ans=Math.max(ans,maxprod);
+        }
+return ans;
     }
 }
