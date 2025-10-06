fb74a257d2960402e5c8d477d273933692598e27
Ujjwal Kriti <tanukriti06@gmail.com>
2025-10-06 21:50:49 +0530

    Time: 2 ms (92.92%), Space: 47.2 MB (67.6%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
@@ -1,12 +1,33 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        for(int i=0;i<nums.length;i++){
-            for(int j=i+1;j<nums.length;j++){
-                if(nums[i]+nums[j]==target){
-                    return new int [] {i+1,j+1};
+
+        // -------------- brute force approach--------------- 
+        // in java we are returning using return new int[] {i+1, j+1}
+
+        // for(int i=0;i<nums.length;i++){
+        //     for(int j=i+1;j<nums.length;j++){
+        //         if(nums[i]+nums[j]==target){
+        //             return new int [] {i+1,j+1};
+        //         }
+        //     }
+        // }
+        // return new int []{} ;
+
+
+        // ---------------------optimal approach ----------------------
+
+            int right=nums.length-1, left=0;
+            while(left<right){
+                if(nums[right]+nums[left]==target){
+                    return new  int [] {left+1,right+1};
+                }else if( nums[left]+nums[right]<target){
+                    left++;
+                         
+                }else{
+                    right--;
+                  
                 }
             }
-        }
-        return new int []{} ;
+               return new  int [] {left+1,right+1};
     }
 }
