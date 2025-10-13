1b367206c9e578e5787ccda4e0080471aec4c0fb
Ujjwal Kriti <tanukriti06@gmail.com>
2025-10-13 10:54:15 +0530

    Time: 15 ms (25.01%), Space: 49.8 MB (88.63%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,18 +1,43 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        int n=nums.length;
-          List<Integer> result = new ArrayList<>();
+
+        // -----------------brute force--------------
+
+        // int n=nums.length;
+        //   List<Integer> result = new ArrayList<>();
      
-        for(int val:nums){
-            int freq=0;
-            for(int el:nums){
-                if(el==val)
-                freq++;
-            }
+        // for(int val:nums){
+        //     int freq=0;
+        //     for(int el:nums){
+        //         if(el==val)
+        //         freq++;
+        //     }
         
-        if(freq>n/3 && !result.contains(val)){
-            result.add(val);
+        // if(freq>n/3 && !result.contains(val)){
+        //     result.add(val);
+        // }
+        // }
+        // return result;
+
+        //---------------------hashtable--------------
+
+        int n=nums.length;
+        HashMap<Integer,Integer>freq=new HashMap<>();
+        List<Integer>result=new  ArrayList<>();
+        // for(int val:nums){
+        //     freq.put(val,freq.getOrDefault(val,0)+1);
+        // }
+        for(int val:nums){
+        if(freq.containsKey(val)){//capital key
+            freq.put(val,freq.get(val)+1);
+        }else{
+            freq.put(val,1);
         }
+    }
+        for(int key : freq.keySet()){
+            if(freq.get(key)>n/3){
+                result.add(key);
+            }
         }
         return result;
     }
