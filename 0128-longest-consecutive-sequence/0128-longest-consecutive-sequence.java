fa4ba9e5b016d96278ae5572ea5a22fb9eb354e1
Ujjwal Kriti <tanukriti06@gmail.com>
2025-10-13 16:13:53 +0530

    Time: 31 ms (58.39%), Space: 67.7 MB (35.44%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -1,25 +1,44 @@
 class Solution {
     public int longestConsecutive(int[] nums) {
-            int n = nums.length;
-        if (n == 0) return 0;
+        // ------------------------------brute force------------
+           // int n = nums.length;
+//         if (n == 0) return 0;
 
-        Arrays.sort(nums);
+//         Arrays.sort(nums);
 
-        int longest = 1;
-        int current = 1;
+//         int longest = 1;
+//         int current = 1;
 
-        for (int i = 1; i < n; i++) {
-            if (nums[i] == nums[i - 1]) {
-                continue; // skip duplicates
-            } else if (nums[i] == nums[i - 1] + 1) {
-                current++; // continue streak
-            } else {
-                longest = Math.max(longest, current);
-                current = 1; // reset streak
-            }
+//         for (int i = 1; i < n; i++) {
+//             if (nums[i] == nums[i - 1]) {
+//                 continue; // skip duplicates
+//             } else if (nums[i] == nums[i - 1] + 1) {
+//                 current++; // continue streak
+//             } else {
+//                 longest = Math.max(longest, current);
+//                 current = 1; // reset streak
+//             }
             
+//         }
+//   return Math.max(longest, current);
+
+    //-------------------------hash-------------------
+       HashSet<Integer>set=new HashSet<>();
+       int longest=0;
+       for(int num:nums){
+        set.add(num);
+       }
+       for(int num:set){
+       if(!set.contains(num-1)){
+        int currentnum=num;
+        int streak=1;
+        while(set.contains(currentnum+1)){
+            currentnum++;
+            streak++;
         }
-  return Math.max(longest, current);
-       
+        longest=Math.max(streak,longest);
+       }
+       }
+       return longest;
     }
 }  
