23f9f9c0707b570d381e43c753152fd2e5bbed38
Ujjwal Kriti <tanukriti06@gmail.com>
2026-01-10 09:30:49 +0530

    Time: 19 ms (10.6%), Space: 47.2 MB (22.74%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        Map<Character,Integer>map=new HashMap<>();
+        for(int i=0;i<magazine.length();i++){
+            char c=magazine.charAt(i);
+            map.put(c,map.getOrDefault(c,0)+1);
+        }
+        //ransome word
+         for(int i=0;i<ransomNote.length();i++){
+            char c=ransomNote.charAt(i);
+            if(!map.containsKey(c)||map.get(c)==0){
+                return false;
+            }
+            //reduce count
+            map.put(c,map.get(c)-1);
+        }
+        return true;
+    }
+}
