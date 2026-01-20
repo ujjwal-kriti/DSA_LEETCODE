e637cf36af06ec9571700cb75126e8a8db77e54e
Ujjwal Kriti <tanukriti06@gmail.com>
2026-01-20 23:45:30 +0530

    Time: 10 ms (13.33%), Space: 46.7 MB (14.99%) - LeetHub

---
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.java
@@ -0,0 +1,20 @@
+class Solution {
+    public String removeOuterParentheses(String s) {
+        String result= "";
+        int depth=0;
+        for(char c:s.toCharArray()){
+            if(c=='('){
+                if(depth>0){
+                    result+=c;
+                }
+                depth++;
+            }else{
+                depth--;
+                if(depth>0){
+                    result+=c;
+                }
+            }
+        }
+        return result;
+    }
+}
