e9978d21f59ee72682f5d98b7c592f50b472329a
Ujjwal Kriti <tanukriti06@gmail.com>
2026-03-11 00:32:51 +0530

    Time: 1 ms (14.39%), Space: 42.3 MB (44.56%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -15,29 +15,63 @@ class Solution {
     //         return right+down;
     //     }
     
-    //
-    public int uniquePaths(int m, int n) {
+    //-----------------using memoization-----------
+    // public int uniquePaths(int m, int n) {
+    //     int[][] dp=new int[m][n];
+    //      for(int i = 0; i < m; i++){
+    //         for(int j = 0; j < n; j++){
+    //             dp[i][j] = -1;
+    //         }
+    //     }
+    //      return f(0, 0, m, n, dp);
+    // }
+    // private int f(int i,int j,int m,int n,int [][]dp){
+    //     if(i==m-1 && j==n-1){
+    //       return 1;
+    //     }
+    //      if(i >= m || j >= n)
+    //         return 0;
+    //          // memoization check
+    //     if(dp[i][j] != -1)
+    //         return dp[i][j];
+
+    //     int right = f(i, j+1, m, n, dp);
+    //     int down = f(i+1, j, m, n, dp);
+
+    //     return dp[i][j] = right + down;
+    // }
+
+    //--------------------------tabulation--------------
+     public int uniquePaths(int m, int n) {
         int[][] dp=new int[m][n];
-         for(int i = 0; i < m; i++){
-            for(int j = 0; j < n; j++){
-                dp[i][j] = -1;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0 && j==0){
+                    dp[i][j]=1;
+                }else{
+                    int up=0;
+                    int left=0;
+                    if(i>0){
+                        up=dp[i-1][j];
+                    }
+                    if(j>0){
+                        left=dp[i][j-1];
+                    }
+                    dp[i][j]=up+left;
+                }
             }
         }
-         return f(0, 0, m, n, dp);
-    }
-    private int f(int i,int j,int m,int n,int [][]dp){
-        if(i==m-1 && j==n-1){
-          return 1;
-        }
-         if(i >= m || j >= n)
-            return 0;
-             // memoization check
-        if(dp[i][j] != -1)
-            return dp[i][j];
-
-        int right = f(i, j+1, m, n, dp);
-        int down = f(i+1, j, m, n, dp);
-
-        return dp[i][j] = right + down;
-    }
+        return dp[m-1][n-1];
+    } 
 }
+// 3️⃣ Both represent the same idea
+// These two relations are actually the same mathematically.
+// Memoization view:
+// paths from (i,j) → end
+// = right + down
+// Tabulation view:
+// paths to reach (i,j)
+// = up + left
+// Just opposite direction thinking.
+// forward → right + down
+// backward → up + left
