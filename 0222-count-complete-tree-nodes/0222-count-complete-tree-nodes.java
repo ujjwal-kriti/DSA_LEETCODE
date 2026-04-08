8caa319119004174a5e3670ce1477efb48071605
Ujjwal Kriti <tanukriti06@gmail.com>
2026-04-08 11:09:51 +0530

    Time: 6 ms (6.87%), Space: 49.9 MB (7.32%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.java
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.java
@@ -16,44 +16,65 @@
 class Solution {
     public int countNodes(TreeNode root) {
     //---------------------------------------------took o(n)tc----------------------
-        // if(root==null)return 0;
-        // int count=0;
-        // Queue<TreeNode>q=new LinkedList<>();
-        // q.add(root);
-        // while(!q.isEmpty()){
-        //     TreeNode node=q.poll();
-        //     count++;
-        //      if(node.left!=null){
-        //             q.add(node.left);
-        //         }
-        //         if(node.right!=null){
-        //             q.add(node.right);
-        //         }
-        // }
-        // return count;
-        if(root==null) return 0;
-        int leftheight=leftheight(root);
-         int rightheight=rightheight(root);
-         if(leftheight==rightheight){
-            return (1<<leftheight)-1;
-         }
-         return 1+countNodes(root.left)+countNodes(root.right);
+       Queue<TreeNode>q=new LinkedList<>();
+       if(root==null) return 0;
+       q.add(root);
+       int count=0;
+       while(!q.isEmpty()){
+        TreeNode node=q.poll();
+        count++;
+            if(node.left!=null){
+                    q.add(node.left);
+            }
+            if(node.right!=null){
+                q.add(node.right);
+            }
+       }
+       return count;
+
     }
-    private int leftheight(TreeNode node){
-        int height=0;
-        while(node!=null){
-            height++;
-            node=node.left;
-        }
-        return height;
-    }
-     private int rightheight(TreeNode node){
-        int height=0;
-        while(node!=null){
-            height++;
-            node=node.right;
-        }
-        return height;
-    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //     if(root==null) return 0;
+    //     int leftheight=leftheight(root);
+    //      int rightheight=rightheight(root);
+    //      if(leftheight==rightheight){
+    //         return (1<<leftheight)-1;
+    //      }
+    //      return 1+countNodes(root.left)+countNodes(root.right);
+    // }
+    // private int leftheight(TreeNode node){
+    //     int height=0;
+    //     while(node!=null){
+    //         height++;
+    //         node=node.left;
+    //     }
+    //     return height;
+    // }
+    //  private int rightheight(TreeNode node){
+    //     int height=0;
+    //     while(node!=null){
+    //         height++;
+    //         node=node.right;
+    //     }
+    //     return height;
+    // }
     
 }
