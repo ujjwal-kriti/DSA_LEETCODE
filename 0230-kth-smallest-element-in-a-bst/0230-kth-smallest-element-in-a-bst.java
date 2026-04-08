a966f66448e979fa7a865052431186561c63298f
Ujjwal Kriti <tanukriti06@gmail.com>
2026-04-08 10:50:17 +0530

    Time: 0 ms (100%), Space: 46.7 MB (58.25%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
@@ -14,20 +14,48 @@
  * }
  */
 class Solution {
-      int count=0;
-     int result=0;
-    public int kthSmallest(TreeNode root, int k) {
-      inorder(root,k);
-      return result;
+    int count=0;
+    int ans=-1;
+   public int kthSmallest(TreeNode root, int k) {
+        inorder (root,k);
+    return ans;
+  } 
+  public void  inorder(TreeNode root, int k){
+    if(root==null) return ;
+    inorder(root.left,k);
+    count++;
+    if(count==k){
+        ans=root.val;
+        return;
     }
-    private void inorder(TreeNode root,int k){
-        if(root==null) return;
-        inorder(root.left,k);
-        count++;
-        if(count==k){
-            result=root.val;
-            return;
-        }
-        inorder(root.right,k);
-    }
+    inorder(root.right,k);
+  }
 }
+
+
+
+
+
+// class Solution {
+
+//     PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+
+//     public int kthSmallest(TreeNode root, int k) {
+//         dfs(root, k);
+//         return pq.peek();
+//     }
+
+//     private void dfs(TreeNode root, int k) {
+
+//         if (root == null) return;
+
+//         pq.add(root.val);
+
+//         if (pq.size() > k) {
+//             pq.poll();
+//         }
+
+//         dfs(root.left, k);
+//         dfs(root.right, k);
+//     }
+// }
