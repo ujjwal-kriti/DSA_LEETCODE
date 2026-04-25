fe24ff4ed9fe93b3f2d56c711831638b0053aef4
Ujjwal Kriti <tanukriti06@gmail.com>
2026-04-25 11:00:48 +0530

    Time: 2 ms (69.54%), Space: 47.6 MB (5.11%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.java
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.java
@@ -1,38 +1,18 @@
 class Solution {
     public int maxProduct(int[] nums) {
-        // brute force approach 
-
-        // int maxp=Integer.MIN_VALUE;
-        // for(int i=0;i<nums.length;i++){
-        //     int product=1;
-        //     for(int j=i;j<nums.length;j++){
-        //         product*=nums[j];
-        //         maxp=Math.max(product,maxp);
-        //     }
-        // }
-        // return maxp;
-
-
-        // optimal approach using kadanes algorithm
-
-        
-        int maxprod=nums[0];
-        int minprod=nums[0];
-        int ans=nums[0];
-
+        int max=nums[0];
+        int maxp=nums[0];
+        int minp=nums[0];
         for(int i=1;i<nums.length;i++){
-            int currprod=nums[i];
-    
-        if(currprod<0){
-            int temp=maxprod;
-            maxprod=minprod;
-            minprod=temp;
+           if(nums[i]<0){
+            int temp=maxp;
+            maxp=minp;
+            minp=temp;
+           }
+            maxp=Math.max(nums[i],maxp*nums[i]);
+            minp=Math.min(nums[i],minp*nums[i]);
+            max=Math.max(maxp,max);
         }
-        maxprod=Math.max(maxprod*currprod,currprod);
-        minprod=Math.min(minprod*currprod,currprod);
-
-        ans=Math.max(ans,maxprod);
-        }
-return ans;
+        return max;
     }
 }
