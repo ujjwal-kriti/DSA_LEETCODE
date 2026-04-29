c284224ed5dbbd85ea47520bf649898e1fce4b60
Ujjwal Kriti <tanukriti06@gmail.com>
2026-04-29 14:45:47 +0530

    Time: 2 ms (90.29%), Space: 44 MB (84.01%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -1,53 +1,42 @@
 class Solution {
     public int orangesRotting(int[][] grid) {
-        int m=grid.length;
-        int n=grid[0].length;
-        Queue<int[]>q=new LinkedList<>();//queue for BFS (store orange positions)
-        int fresh=0;
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
+       int n=grid.length;
+       int m=grid[0].length;
+       int fresh=0;
+       Queue<int[]>q= new LinkedList<>();
+       for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
                 if(grid[i][j]==2){
-                    q.offer(new int[]{i,j});//found rotten orange ,put its position in queue
+                    q.offer(new int[]{i,j});
                 }
                 if(grid[i][j]==1){
-                    //found fresh orange,increase count
                     fresh++;
                 }
             }
-        }
+       }
+       //bfs
+       int[][] dir={{-1,0},{1,0},{0,-1},{0,1}};
+       int min=0;
+        while(!q.isEmpty() && fresh>0){
+            int size=q.size();
+            for(int i=0;i<size;i++){
+                int [] curr=q.poll();
+                int r=curr[0];
+                int c=curr[1];
+                for(int[] d:dir){
+                    int nr=r+d[0];
+                    int nc=c+d[1];
+                     if (nr >= 0 && nc >= 0 && nr < n && nc < m && grid[nr][nc] == 1) {
+                          grid[nr][nc] = 2;
+                          fresh--;
+                           q.offer(new int[]{nr, nc});
+                     }
 
-        //if no fresh orange is presnt ,answer is 0 minutes 
-        if(fresh==0) return 0;
-
-        int minutes=0;
-        int[][] dirs = {{-1,0},{1,0},{0,-1},{0,1}};
-        //bfs
-        while(!q.isEmpty()){
-            int size=q.size();// number of rotten oranges currently in the queue
-            // size means 1 BFS "level" = 1 minute
-            boolean rottedthisminute=false;
-            for(int k=0;k<size;k++){
-                int []cell=q.poll();//take one rotten orange
-                int r=cell[0];
-                int c=cell[1];
-                for(int d=0;d<4;d++){//check its four direction
-                    int nr=r+dirs[d][0];
-                    int nc=c+dirs[d][1];
-                    if(nr >= 0 && nr < m && nc >= 0 && nc < n && grid[nr][nc] == 1){//“Is neighbour inside the grid?”, grid[nr][nc]==1 means fresh orange
-
-                    grid[nr][nc]=2;
-                    fresh--;
-                    q.offer(new int[]{nr,nc});
-                    rottedthisminute=true;
-
-                    }
                 }
             }
-            if(rottedthisminute)minutes++;
-            
+            min++;
         }
-        if(fresh==0)return minutes;
-        return -1;
+        return fresh == 0 ? min : -1;
     }
 }
 // 4-direction movement (Up, Down, Left, Right)
