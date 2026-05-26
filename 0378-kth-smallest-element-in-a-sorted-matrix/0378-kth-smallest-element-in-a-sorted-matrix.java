59b1b53e4364239d89c1ba5084725b3ceb45e3d9
Ujjwal Kriti <tanukriti06@gmail.com>
2026-05-26 19:36:52 +0530

    Time: 28 ms (12.08%), Space: 51.2 MB (53.88%) - LeetHub

--- 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.java
+++ 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.java
@@ -5,12 +5,12 @@ class Solution {
         PriorityQueue<Integer>pq=new PriorityQueue<>(Collections.reverseOrder());
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                pq.offer(matrix[i][j]);
-                if(pq.size()>k){
-                    pq.poll();
-                }
+            pq.add(matrix[i][j]);
+            
+            if(pq.size()>k){
+                pq.poll();
             }
-
+            }
         }
         return pq.peek();
     }
