887c357d90bcdb7944034f5c725ebed2e43041f3
Ujjwal Kriti <tanukriti06@gmail.com>
2026-06-09 08:48:07 +0530

    Time: 32 ms (16.93%), Space: 91 MB (19.25%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -1,43 +1,23 @@
 class Solution {
     public int findDuplicate(int[] nums) {
-
-        // -----------------------this is a brute force approach like time limit exceedded 
-        // int n=nums.length;
-        // for(int i=0;i<n;i++){
-        //     for(int j=i+1;j<n;j++){
-        //         if(nums[i]==nums[j]){
-        //             return nums[i];
-        //         }
-        //     }
-        // }
-        // return n ;
-
-
-        //------------using count method ---------
-
-        // int n=nums.length;
-        // int [] count=new int [n+1];
-        // for(int i=0;i<n;i++){
-        //     count[nums[i]]++;
-        //     if(count[nums[i]]>1){
-        //         return nums[i];
-        //     }
-        // }
-
-        // return n;
-
         // -----------using hash ----------------
-        // here set is used  because we want to store unique number 
+        // HashSet<Integer>set=new HashSet<>();
+        // for(int num:nums){
+        //     if(set.contains(num)){
+        //         return num;
+        //     }
+        //     set.add(num);
+        // }
+        // return -1;
 
-        Set<Integer>set=new HashSet<>();
-        int len=nums.length;
-        for(int i=0;i<len;i++){
-        if(!set.add (nums[i])){
-            return nums[i];
+        HashMap<Integer,Integer>map=new HashMap<>();
+        for(int num:nums){
+            map.put(num,map.getOrDefault(num,0)+1);
+            if(map.get(num)>1){
+                return num;
+            }
         }
-        }
-        return len;
-        
+        return -1;
 
     }
 }
